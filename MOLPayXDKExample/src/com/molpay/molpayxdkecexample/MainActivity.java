98ad96cb2793599a4ee1dcd9165fa8cd5d27ceae
Admin <Admin>
2016-06-24 12:25:41 +0800

    add custom css

--- MOLPayXDKExample/src/com/molpay/molpayxdkecexample/MainActivity.java
+++ MOLPayXDKExample/src/com/molpay/molpayxdkecexample/MainActivity.java
@@ -56,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
         paymentDetails.put(MOLPayActivity.mp_transaction_id, "");//6713246
         paymentDetails.put(MOLPayActivity.mp_request_type, "");
         
+        //String binlock[] = {"123456","234567"};
+        //paymentDetails.put(MOLPayActivity.mp_bin_lock, binlock);
+        //paymentDetails.put(MOLPayActivity.mp_bin_lock_err_msg, "Wrong BIN format");
+        //paymentDetails.put(MOLPayActivity.mp_is_escrow, "");
+        //paymentDetails.put(MOLPayActivity.mp_filter, "1");
+        //paymentDetails.put(MOLPayActivity.mp_custom_css_url, "file:///android_asset/custom.css");
+        
         Intent intent = new Intent(MainActivity.this, MOLPayActivity.class);
         intent.putExtra(MOLPayActivity.MOLPayPaymentDetails, paymentDetails);
         startActivityForResult(intent, MOLPayActivity.MOLPayXDK);
